150979138fc327eb181d47b67a9b0330b2f27fc0
Kannan G <kannan.ganesan@thoughtworks.com>
2020-12-04 16:54:22 +0530

    get kubeconfig shell added

---
+++ create-kube-config-from-svcaccount.sh
@@ -0,0 +1,29 @@
+# your server name goes here
+server=https://143.110.254.153:6443
+# the name of the secret containing the service account token goes here
+name=endgame-token-jwwjp
+
+ca=$(kubectl get secret/$name -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl get secret/$name -o jsonpath='{.data.token}' | base64 --decode)
+namespace=$(kubectl get secret/$name -o jsonpath='{.data.namespace}' | base64 --decode)
+
+echo "
+apiVersion: v1
+kind: Config
+clusters:
+- name: default-cluster
+  cluster:
+    certificate-authority-data: ${ca}
+    server: ${server}
+contexts:
+- name: default-context
+  context:
+    cluster: default-cluster
+    namespace: default
+    user: default-user
+current-context: default-context
+users:
+- name: default-user
+  user:
+    token: ${token}
+" > sa.conf
